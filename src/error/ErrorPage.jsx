269879e064d6607dcfbce48aaa039910db15bc94
dashluu <dashluu9121997@gmail.com>
2024-09-04 15:42:02 -0700

    Redesigned error page.

--- src/error/ErrorPage.jsx
+++ src/error/ErrorPage.jsx
@@ -1,8 +1,23 @@
+import "./ErrorPage.scss";
+import NavBar from "../navbar/NavBar.jsx";
+import {useNavigate} from "react-router-dom";
+
 const ErrorPage = () => {
+    const navigate = useNavigate();
+
     return (
         <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
+            <NavBar/>
+            <div className="error-container">
+                <div className="error-title">Page not found</div>
+                <p className="error-message">Sorry, an unexpected error has occurred</p>
+                <div className="error-button-container">
+                    <button className="error-button" onClick={() => navigate("/")}>
+                        <span className="material-symbols-outlined">home</span>
+                        <span>Go to Home</span>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
