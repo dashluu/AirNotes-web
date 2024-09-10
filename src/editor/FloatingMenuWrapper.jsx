6c07928f8d1fcb7e52ce9b6ddc63903b961483ce
dashluu <dashluu9121997@gmail.com>
2024-09-09 17:54:29 -0700

    Added select options to floating menu.

--- src/editor/FloatingMenuWrapper.jsx
+++ src/editor/FloatingMenuWrapper.jsx
@@ -10,6 +10,15 @@ function FloatingMenuWrapper({editor}) {
         <FloatingMenu editor={editor} tippyOptions={{duration: 100}}>
             <div className="editor-menu">
                 <HeadingSelect editor={editor}/>
+                <EditorMenuButton title="Bold" icon="format_bold"
+                                  className={editor.isActive("bold") ? "editor-menu-button-active" : "editor-menu-button"}
+                                  click={() => editor.chain().focus().toggleBold().run()}/>
+                <EditorMenuButton title="Italic" icon="format_italic"
+                                  className={editor.isActive("italic") ? "editor-menu-button-active" : "editor-menu-button"}
+                                  click={() => editor.chain().focus().toggleItalic().run()}/>
+                <EditorMenuButton title="Underline" icon="format_underlined"
+                                  className={`underline-button ${editor.isActive("underline") ? "editor-menu-button-active" : "editor-menu-button"}`}
+                                  click={() => editor.chain().focus().toggleUnderline().run()}/>
                 <AlignSelect editor={editor}/>
                 <ListSelect editor={editor}/>
                 <EditorMenuButton title="Code Block" icon="code"
